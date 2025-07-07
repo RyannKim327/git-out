49565be13bb9f38029d2ab0d97efed47578eaf32
RyannKim327 <rksesgundo123@gmail.com>
2025-07-07 19:15:13 +0000

    7-7-2025 19:14 [Wag na ipaglaban kung gusto, basta masaya sya doon, don na sya]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,13 +1,9 @@
-function isArraySortedAscending(arr: number[]): boolean {
-    return arr.every((value, index) => {
-        if (index === 0) return true; // The first element is always considered sorted
-        return value >= arr[index - 1]; // Check if the current element is greater than or equal to the previous one
-    });
-}
-
-// Example usage:
-const sortedArray = [1, 2, 3, 4, 5];
-const unsortedArray = [1, 3, 2, 4, 5];
-
-console.log(isArraySortedAscending(sortedArray)); // Output: true
-console.log(isArraySortedAscending(unsortedArray)); // Output: false
+const str: string = " This is a test string. ";
+const noSpaces: string = str.replace(/\s+/g, '');
+console.log(noSpaces); // Output: "Thisisateststring."
+const str: string = " This is a test string. ";
+const noSpaces: string = str.split(' ').join('');
+console.log(noSpaces); // Output: "Thisisateststring."
+const str: string = " This is a test string. ";
+const noSpaces: string = str.replaceAll(' ', '');
+console.log(noSpaces); // Output: "Thisisateststring."
