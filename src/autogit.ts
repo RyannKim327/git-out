2c7f812ff4203738bdff5f6d0eea8b8c444a19c4
RyannKim327 <rksesgundo123@gmail.com>
2025-10-14 23:00:51 +0000

    10-14-2025 23:0 [May pinagsamahan man tayo, pero tingin ko wala na un]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,22 @@
-// May error sa API
+const str = "Hello World with spaces";
+const noSpaces = str.replace(/\s/g, '');
+
+console.log(noSpaces); // "HelloWorldwithspaces"
+const str = "Hello World with spaces";
+const noSpaces = str.replaceAll(' ', '');
+
+console.log(noSpaces); // "HelloWorldwithspaces"
+function removeSpaces(str: string): string {
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== ' ') {
+            result += str[i];
+        }
+    }
+    return result;
+}
+
+const str = "Hello World with spaces";
+console.log(removeSpaces(str)); // "HelloWorldwithspaces"
+const str: string = "Hello World";
+const noSpaces: string = str.replace(/\s/g, ''); // TypeScript knows this is a string
