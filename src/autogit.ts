37268582a4aa28e328f82346839335284c72c2eb
RyannKim327 <rksesgundo123@gmail.com>
2025-10-18 10:01:27 +0000

    10-18-2025 10:0 [Hindi naman daw ako pangit, pero ayaw nya kaya wag na]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,14 @@
-// May error sa API
+let firstName = "Ada";
+let lastName = "Lovelace";
+let fullName = firstName + " " + lastName;
+console.log(fullName); // "Ada Lovelace"
+let firstName = "Ada";
+let lastName = "Lovelace";
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName); // "Ada Lovelace"
+let age = 36;
+console.log(`She is ${age} years old.`);
+let firstName = "Ada";
+let lastName = "Lovelace";
+let fullName = firstName.concat(" ", lastName);
+console.log(fullName); // "Ada Lovelace"
