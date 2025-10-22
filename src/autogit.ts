31f3de8001a9d20189df0e349d4587bf156974b8
RyannKim327 <rksesgundo123@gmail.com>
2025-10-22 08:02:55 +0000

    10-22-2025 8:2 [Sanaol Santol]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,16 +1,1 @@
-function getRandomInt(min: number, max: number): number {
-  const minCeil = Math.ceil(min);
-  const maxFloor = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloor - minCeil + 1)) + minCeil;
-}
-
-// Example usage:
-const num = getRandomInt(10, 20);
-console.log(num); // Could be anything from 10 to 20 inclusive
-function getRandomFloat(min: number, max: number): number {
-  return Math.random() * (max - min) + min;
-}
-
-// Example usage:
-const num = getRandomFloat(1.5, 4.2);
-console.log(num); // Between 1.5 and 4.2
+// May error sa API
