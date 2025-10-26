1a4d0ea0d9f31f8155bddc86f7487137d6dd3bbb
RyannKim327 <rksesgundo123@gmail.com>
2025-10-26 17:01:05 +0000

    10-26-2025 17:1 [Sorry kung isang araw, sino ka na lang sa akin]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,3 @@
-// May error sa API
+const numbers: number[] = [3, 7, 2, 9, 5];
+const max = Math.max(...numbers);   // 9
+const max = numbers.length ? Math.max(...numbers) : undefined; // or any fallback you prefer
