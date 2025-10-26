a12085d6944c8d9fad6cc326a2a88a412fb32516
RyannKim327 <rksesgundo123@gmail.com>
2025-10-26 19:02:51 +0000

    10-26-2025 19:2 [Mukhang kailangan ko nang i-practice na si sya ma miss]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,11 +1,1 @@
-function removeVowels(str: string): string {
-  return str.replace(/[aeiouAEIOU]/g, '');
-}
-
-console.log(removeVowels("Hello World")); // "Hll Wrld"
-function removeVowels(str: string, vowels: string = "aeiouAEIOU"): string {
-  const pattern = new RegExp(`[${vowels}]`, 'g');
-  return str.replace(pattern, '');
-}
-
-console.log(removeVowels("TypeScript", "aeiouyAEIOUY")); // "TpScrpt"
+// May error sa API
