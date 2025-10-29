d67dc460a23066418712bbe659e1dc3166e808eb
RyannKim327 <rksesgundo123@gmail.com>
2025-10-29 10:01:57 +0000

    10-29-2025 10:1 [Sorry kung isang araw, sino ka na lang sa akin]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,13 @@
-// May error sa API
+const str = 'The quick brown fox';
+const needle = 'brown';
+
+if (str.includes(needle)) {
+  console.log('Found!');
+}
+if (str.indexOf(needle) !== -1) {
+  console.log('Found!');
+}
+const regex = /brown/i;   // the i flag makes it case-insensitive
+if (regex.test(str)) {
+  console.log('Found!');
+}
