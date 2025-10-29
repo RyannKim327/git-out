9569c1f0ec4306e76d09e2cd85b5142db173339f
RyannKim327 <rksesgundo123@gmail.com>
2025-10-29 10:01:57 +0000

    10-29-2025 10:1 [Paano yan, lugi nanaman tayo]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,13 +1,1 @@
-const str = 'The quick brown fox';
-const needle = 'brown';
-
-if (str.includes(needle)) {
-  console.log('Found!');
-}
-if (str.indexOf(needle) !== -1) {
-  console.log('Found!');
-}
-const regex = /brown/i;   // the i flag makes it case-insensitive
-if (regex.test(str)) {
-  console.log('Found!');
-}
+// May error sa API
