bc932f7d8392d1fcb2ad6d42182a809321c6de72
RyannKim327 <rksesgundo123@gmail.com>
2025-10-29 02:26:57 +0000

    10-29-2025 2:26 [Sorry kung isang araw, sino ka na lang sa akin]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,12 @@
-// May error sa API
+let str: string = "42";
+let num: number = parseInt(str, 10); // 10 is for base-10
+console.log(num); // 42
+let str: string = "42";
+let num: number = Number(str);
+console.log(num); // 42
+let str: string = "42";
+let num: number = +str;
+console.log(num); // 42
+let str: string = "42.9";
+let num: number = Math.floor(Number(str)); // or Math.round, Math.trunc
+console.log(num); // 42
