a3d277ab45734dfba0d35106f6b6f6423b01a914
RyannKim327 <rksesgundo123@gmail.com>
2025-10-29 07:00:53 +0000

    10-29-2025 7:0 [Wag na ipaglaban kung gusto, basta masaya sya doon, don na sya]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,9 +1,1 @@
-let numbers: number[] = [42, 5, 18, 100, 7];
-
-// Ascending order
-numbers.sort((a, b) => a - b);
-console.log(numbers); // [5, 7, 18, 42, 100]
-
-// Descending order
-numbers.sort((a, b) => b - a);
-console.log(numbers); // [100, 42, 18, 7, 5]
+// May error sa API
