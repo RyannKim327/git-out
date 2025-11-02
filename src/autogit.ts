cadb8c0230913585b8ff4c23563043085c62bc5a
RyannKim327 <rksesgundo123@gmail.com>
2025-11-02 13:14:24 +0000

    11-2-2025 13:14 [Sorry kung isang araw, sino ka na lang sa akin]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,14 @@
-// May error sa API
+function reverseString(str: string): string {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverseString('hello')); // "olleh"
+function reverseStringManual(str: string): string {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+console.log(reverseStringManual('TypeScript')); // "tpircSpeyT"
