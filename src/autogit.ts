49f209fe8b75a9d4cb7a9310cc926104902e779f
RyannKim327 <rksesgundo123@gmail.com>
2025-11-04 16:01:49 +0000

    11-4-2025 16:1 [Buti na lang umiwas na, kasi meron na pala]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,18 +1,1 @@
-const str = "Hello World!  How are you?";
-const result = str.replace(/\s/g, '');
-console.log(result); // Output: "HelloWorld!Howareyou?"
-const str = "Hello World!  How are you?";
-const result = str.replace(/ /g, '');
-console.log(result); // Output: "HelloWorld!Howareyou?"
-const str = "Hello World!";
-const result = str.split(' ').join('');
-console.log(result); // Output: "HelloWorld!"
-const removeSpaces = (input: string, removeAllWhitespace = true): string => {
-  return removeAllWhitespace 
-    ? input.replace(/\s/g, '')   // Remove all whitespace
-    : input.replace(/ /g, '');   // Remove only spaces
-};
-
-// Usage
-console.log(removeSpaces("Hello\t\nWorld!")); // Output: "HelloWorld!"
-console.log(removeSpaces("Hello\t\nWorld!", false)); // Output: "Hello\t\nWorld!"
+// May error sa API
