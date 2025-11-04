f00e53f4af0df93b5b58acffd4887fbeef99c1e6
RyannKim327 <rksesgundo123@gmail.com>
2025-11-04 17:01:01 +0000

    11-4-2025 17:1 [Saan aabot ang bente pesos mo?]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,14 +1,1 @@
-function reverseString(str: string): string {
-  return str.split('').reverse().join('');
-}
-
-console.log(reverseString('hello')); // "olleh"
-function reverseStringManual(str: string): string {
-  let reversed = '';
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
-  }
-  return reversed;
-}
-
-console.log(reverseStringManual('TypeScript')); // "tpircSpeyT"
+// May error sa API
