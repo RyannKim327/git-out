00b324adb26a27256124205fedb8eb9126148b7c
RyannKim327 <rksesgundo123@gmail.com>
2025-11-05 12:06:02 +0000

    11-5-2025 12:6 [Wag nang umulit, at mukhang hindi na ako uulit pa]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,4 +1,1 @@
-let fruits = ["apple", "banana", "cherry"];
-fruits = fruits.filter(fruit => fruit !== "banana");
-const indexToRemove = 1; // "banana" is at index 1
-fruits.splice(indexToRemove, 1);
+// May error sa API
