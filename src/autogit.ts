82feedafdd5964d63e8607fc87035f1322fe6b8d
RyannKim327 <rksesgundo123@gmail.com>
2025-11-05 02:24:54 +0000

    11-5-2025 2:24 [Mukhang kailangan ko nang i-practice na si sya ma miss]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,5 +1,1 @@
-const numbers: number[] = [3, 7, 2, 9, 5];
-const max = Math.max(...numbers);
-console.log(max); // 9
-const max = numbers.length > 0 ? Math.max(...numbers) : undefined;
-const max = numbers.reduce((m, v) => (v > m ? v : m), -Infinity);
+// May error sa API
