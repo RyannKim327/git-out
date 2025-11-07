2753b30700bb653f67a428dfbf07d93bbf6398e7
RyannKim327 <rksesgundo123@gmail.com>
2025-11-07 00:41:47 +0000

    11-7-2025 0:41 [Pinasaasa nya lang ako.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,7 @@
-// May error sa API
+const original = "Hello World This Is Typescript";
+
+// Remove all spaces
+const noSpaces = original.replace(/\s/g, "");
+
+console.log(noSpaces); // "HelloWorldThisIsTypescript"
+const onlySpacesRemoved = original.replace(/ /g, "");
