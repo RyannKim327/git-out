b82c7093f009f72dd6c215b70a66f3cb0bfd022d
RyannKim327 <rksesgundo123@gmail.com>
2025-11-07 02:26:41 +0000

    11-7-2025 2:26 [Pinasaasa nya lang ako.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,17 +1,1 @@
-function gnomeSort(arr: number[]): number[] {
-    let i = 0;
-    while (i < arr.length) {
-        if (i === 0 || arr[i] >= arr[i - 1]) {
-            i++;
-        } else {
-            // Swap arr[i] and arr[i - 1]
-            [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]];
-            i--;
-        }
-    }
-    return arr;
-}
-
-// Example usage:
-const example = [5, 2, 9, 1, 5, 6];
-console.log(gnomeSort(example)); // Output: [1, 2, 5, 5, 6, 9]
+// May error sa API
