b55f4273d7bb328de9aae0ba6c755a018761f78c
RyannKim327 <rksesgundo123@gmail.com>
2025-11-07 00:41:47 +0000

    11-7-2025 0:41 [Saan aabot ang bente pesos mo?]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,7 +1,1 @@
-const original = "Hello World This Is Typescript";
-
-// Remove all spaces
-const noSpaces = original.replace(/\s/g, "");
-
-console.log(noSpaces); // "HelloWorldThisIsTypescript"
-const onlySpacesRemoved = original.replace(/ /g, "");
+// May error sa API
