831441eb2a8b3d1663aa6a5be14c95e36038fb11
RyannKim327 <rksesgundo123@gmail.com>
2025-11-08 07:05:12 +0000

    11-8-2025 7:3 [Sanaol Santol]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,26 @@
-// May error sa API
+function longestCommonPrefix(strs: string[]): string {
+    if (strs.length === 0) return "";
+    for (let i = 0; i < strs[0].length; i++) {
+        const char = strs[0][i];
+        for (let j = 1; j < strs.length; j++) {
+            if (i === strs[j].length || strs[j][i] !== char) {
+                return strs[0].substring(0, i);
+            }
+        }
+    }
+    return strs[0];
+}
+function longestCommonPrefix(strs: string[]): string {
+    if (strs.length === 0) return "";
+    let prefix = strs[0];
+    for (const current of strs) {
+        while (current.indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+            if (prefix === "") return "";
+        }
+    }
+    return prefix;
+}
+console.log(longestCommonPrefix(["flower", "flow", "flight"])); // "fl"
+console.log(longestCommonPrefix(["dog", "racecar", "car"]));    // ""
+console.log(longestCommonPrefix(["interstellar", "internet", "interval"])); // "inte"
