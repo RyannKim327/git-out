affe2e5acfd4fc5bfe05a0326ce9843b704d2c62
RyannKim327 <rksesgundo123@gmail.com>
2025-11-13 03:39:43 +0000

    11-13-2025 3:39 [Sorry kung isang araw, sino ka na lang sa akin]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,9 +1,1 @@
-const str = "123";
-const num = parseInt(str, 10); // Always include the radix (10 for decimal)
-console.log(num); // 123 (number)
-const str = "456";
-const num = Number(str);
-console.log(num); // 456 (number)
-const str = "789";
-const num = +str;
-console.log(num); // 789 (number)
+// May error sa API
