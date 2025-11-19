c36c19cdd6e730831b547e0592e1b1c1a24e54b8
RyannKim327 <rksesgundo123@gmail.com>
2025-11-19 19:00:45 +0000

    11-19-2025 19:0 [May pinagsamahan man tayo, pero tingin ko wala na un]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,15 +1,1 @@
-function reverseWords(str: string): string {
-  return str.split(' ').reverse().join(' ');
-}
-
-const input = "Hello world this is TypeScript";
-const output = reverseWords(input);
-
-console.log(output); 
-// "TypeScript is this world Hello"
-function reverseWordsClean(str: string): string {
-  return str.trim().split(/\s+/).reverse().join(' ');
-}
-
-console.log(reverseWordsClean("   Hello    world   this is   TypeScript   "));
-// "TypeScript is this world Hello"
+// May error sa API
