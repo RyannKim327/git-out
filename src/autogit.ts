12b5db2377a52a7f18051f2406e0c33008e23cfd
RyannKim327 <rksesgundo123@gmail.com>
2025-11-20 05:01:58 +0000

    11-20-2025 5:1 [Sanaol Santol]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,11 @@
-// May error sa API
+function removeVowels(str: string): string {
+  return str.replace(/[aeiouAEIOU]/g, '');
+}
+
+console.log(removeVowels("Hello World")); // "Hll Wrld"
+function removeVowels(str: string, vowels: string = "aeiouAEIOU"): string {
+  const pattern = new RegExp(`[${vowels}]`, 'g');
+  return str.replace(pattern, '');
+}
+
+console.log(removeVowels("TypeScript", "aeiouyAEIOUY")); // "TpScrpt"
