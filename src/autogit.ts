02db6fb8b2bbe353efe6ae4ab5ce9dc6bbb1a079
RyannKim327 <rksesgundo123@gmail.com>
2025-11-21 15:01:22 +0000

    11-21-2025 15:1 [Sorry kung isang araw, sino ka na lang sa akin]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,4 @@
-// May error sa API
+const reversed = (str: string): string => [...str].reverse().join('');
+console.log(reverse('hello'));     // "olleh"
+console.log(reverse('mañana'));    // "anañam"
+console.log(reverse('👩🏽‍🚀'));      // "🚀‍🏽👩"  (correctly keeps the emoji together)
