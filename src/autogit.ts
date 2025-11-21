4d294019feb93f42f640351e7a69443ad517fa9f
RyannKim327 <rksesgundo123@gmail.com>
2025-11-21 16:01:20 +0000

    11-21-2025 16:0 [May pinagsamahan man tayo, pero tingin ko wala na un]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,4 +1,1 @@
-const reversed = (str: string): string => [...str].reverse().join('');
-console.log(reverse('hello'));     // "olleh"
-console.log(reverse('mañana'));    // "anañam"
-console.log(reverse('👩🏽‍🚀'));      // "🚀‍🏽👩"  (correctly keeps the emoji together)
+// May error sa API
