bf690d95313f5da3af56d0b5dc084580bf76c288
RyannKim327 <rksesgundo123@gmail.com>
2025-11-25 08:14:25 +0000

    11-25-2025 8:14 [Saan aabot ang bente pesos mo?]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,14 +1,1 @@
-let firstName = "Ada";
-let lastName = "Lovelace";
-let fullName = firstName + " " + lastName;
-console.log(fullName); // "Ada Lovelace"
-let firstName = "Ada";
-let lastName = "Lovelace";
-let fullName = `${firstName} ${lastName}`;
-console.log(fullName); // "Ada Lovelace"
-let age = 36;
-console.log(`She is ${age} years old.`);
-let firstName = "Ada";
-let lastName = "Lovelace";
-let fullName = firstName.concat(" ", lastName);
-console.log(fullName); // "Ada Lovelace"
+// May error sa API
