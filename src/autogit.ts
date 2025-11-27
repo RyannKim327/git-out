4f34cbeb42e18807c353b6790dcb7cb1812e55c8
RyannKim327 <rksesgundo123@gmail.com>
2025-11-27 00:42:18 +0000

    11-27-2025 0:41 [Hindi daw, sheesh]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,5 +1,1 @@
-function reverseWords(str: string): string {
-    return str.trim().split(/\s+/).reverse().join(' ');
-}
-console.log(reverseWords(" Hello   world  TypeScript! ")); 
-// Output: "TypeScript! world Hello"
+// May error sa API
