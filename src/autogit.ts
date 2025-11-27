657590e3c28e4b6e0f68b1f56fee3264676b232b
RyannKim327 <rksesgundo123@gmail.com>
2025-11-27 08:03:06 +0000

    11-27-2025 8:3 [Mukhang kailangan ko nang i-practice na si sya ma miss]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,9 @@
-// May error sa API
+function areAnagrams(str1: string, str2: string): boolean {
+  const normalize = (str: string) =>
+    str.replace(/\s+/g, '').toLowerCase().split('').sort().join('');
+  
+  return normalize(str1) === normalize(str2);
+}
+console.log(areAnagrams('listen', 'silent')); // true
+console.log(areAnagrams('rail safety', 'fairy tales')); // true
+console.log(areAnagrams('hello', 'world')); // false
