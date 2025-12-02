b5831849c5a4c8ac3821e1a0cc6c1cc42855cf34
RyannKim327 <rksesgundo123@gmail.com>
2025-12-02 19:10:06 +0000

    12-2-2025 19:10 [Hindi naman daw ako pangit, pero ayaw nya kaya wag na]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,4 @@
-// May error sa API
+let fruits = ["apple", "banana", "cherry"];
+fruits = fruits.filter(fruit => fruit !== "banana");
+const indexToRemove = 1; // "banana" is at index 1
+fruits.splice(indexToRemove, 1);
