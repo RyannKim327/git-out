befbb30b153a9b724637439f054b480aa698efe2
RyannKim327 <rksesgundo123@gmail.com>
2025-12-02 13:32:20 +0000

    12-2-2025 13:31 [Buti na lang umiwas na, kasi meron na pala]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,12 @@
-// May error sa API
+// Removes ONLY standard space characters (not tabs/newlines)
+const str1: string = "Hello World!";
+const noSpaces1: string = str1.replace(/ /g, ''); 
+console.log(noSpaces1); // "HelloWorld!"
+
+// Removes ALL whitespace (spaces, tabs, newlines, etc.)
+const str2: string = "Type\tScript\n is fun!";
+const noWhitespace: string = str2.replace(/\s/g, ''); 
+console.log(noWhitespace); // "TypeScriptisfun!"
+const str: string = "Remove all spaces";
+const result: string = str.split(' ').join('');
+console.log(result); // "Removeallspaces"
