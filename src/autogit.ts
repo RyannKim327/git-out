aae6f323f27d4513ffef4de330ff75caf13ff33c
RyannKim327 <rksesgundo123@gmail.com>
2025-12-05 08:13:03 +0000

    12-5-2025 8:12 [Hindi daw, sheesh]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,8 +1,1 @@
-function mean(numbers: number[]): number {
-  if (numbers.length === 0) throw new Error('mean of empty array is undefined');
-  return numbers.reduce((sum, val) => sum + val, 0) / numbers.length;
-}
-
-/* ---- usage ---- */
-const data = [3, 7, 9, 11];
-console.log(mean(data)); // 7.5
+// May error sa API
