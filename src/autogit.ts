6e3ea31a7f95b970e4936536812c548039041cef
RyannKim327 <rksesgundo123@gmail.com>
2025-12-07 09:02:02 +0000

    12-7-2025 9:2 [Wag nang umulit, at mukhang hindi na ako uulit pa]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,5 +1,1 @@
-const stringWithSpaces = "Hello World 2024";
-const noSpaces = stringWithSpaces.replace(/ /g, ''); // "HelloWorld2024"
-const noSpaces = stringWithSpaces.replaceAll(' ', ''); // "HelloWorld2024"
-const stringWithWhitespace = "Hello\tWorld\n2024";
-const noWhitespace = stringWithWhitespace.replace(/\s/g, ''); // "HelloWorld2024"
+// May error sa API
