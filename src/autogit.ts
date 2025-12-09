6d26df89f51871da31d65e4cb11c679c3fd9874c
RyannKim327 <rksesgundo123@gmail.com>
2025-12-09 15:04:26 +0000

    12-9-2025 15:4 [Saan aabot ang bente pesos mo?]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,9 +1,1 @@
-const numbers: number[] = [5, 2, 9, 1, 5, 6];
-
-// 1. Ascending
-numbers.sort((a, b) => a - b);
-console.log(numbers); // [1, 2, 5, 5, 6, 9]
-
-// 2. Descending
-numbers.sort((a, b) => b - a);
-console.log(numbers); // [9, 6, 5, 5, 2, 1]
+// May error sa API
