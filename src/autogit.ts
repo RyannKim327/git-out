59e984173191a5f5e1b1d5ae7afdeeebda029543
RyannKim327 <rksesgundo123@gmail.com>
2025-12-10 22:00:44 +0000

    12-10-2025 22:0 [Buti na lang umiwas na, kasi meron na pala]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,11 @@
-// May error sa API
+const text = 'Hello, world!';
+const needle = 'world';
+
+if (text.includes(needle)) {
+  console.log('Found!');
+}
+if (text.indexOf(needle) !== -1) {
+  console.log('Found!');
+}
+const found = /world/i.test(text);   // i = ignore case
+console.log(found);
