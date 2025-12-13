62ac66f0057ac2cb4d78094cab9816925fd5d3ca
RyannKim327 <rksesgundo123@gmail.com>
2025-12-13 22:00:38 +0000

    12-13-2025 22:0 [Pinasaasa nya lang ako.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,10 +1,1 @@
-const str = 'hello world';
-const needle = 'world';
-
-if (str.includes(needle)) {
-  console.log('found');
-}
-if (str.indexOf(needle) !== -1) {
-  console.log('found');
-}
-const found = /world/i.test(str);   // i = ignore case
+// May error sa API
