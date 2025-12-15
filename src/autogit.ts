e83ccf17a2b63ca12c6774ba9a6525e2133df0a2
RyannKim327 <rksesgundo123@gmail.com>
2025-12-15 04:34:15 +0000

    12-15-2025 4:34 [Wag na ipaglaban kung gusto, basta masaya sya doon, don na sya]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,7 +1,1 @@
-const mean = (nums: number[]): number =>
-  nums.length === 0 ? NaN : nums.reduce((a, b) => a + b, 0) / nums.length;
-
-/* ---------- usage ---------- */
-console.log(mean([2, 4, 6])); // 4
-console.log(mean([]));        // NaN  (or throw if you prefer)
-throw new Error("mean of empty list is undefined");
+// May error sa API
