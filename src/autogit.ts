ba934a1b92731dc49068d8a0456b34aca7d61cd5
RyannKim327 <rksesgundo123@gmail.com>
2025-12-15 21:00:56 +0000

    12-15-2025 21:0 [Buti na lang umiwas na, kasi meron na pala]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,13 +1,1 @@
-const str = "42";
-const n: number = Number(str);   // 42
-// or
-const n2: number = +str;        // 42 (unary plus)
-const hex = "ff";
-const n = parseInt(hex, 16);     // 255
-const str = "123abc";
-const n = Number(str);           // NaN
-const safe = Number.isNaN(n) ? 0 : n; // 0
-const str = "3.14";
-const int = Number.parseInt(str, 10); // 3
-// or
-const int2 = Math.trunc(Number(str)); // 3
+// May error sa API
