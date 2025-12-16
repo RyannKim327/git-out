05fb91d29e11aeec9626ecee2f1c68ab183b5aa4
RyannKim327 <rksesgundo123@gmail.com>
2025-12-16 03:54:40 +0000

    12-16-2025 3:54 [Hindi daw, sheesh]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,13 +1,1 @@
-const numbers = [3, 5, 3, 7, 5, 9];
-const unique = [...new Set(numbers)];   // [3, 5, 7, 9]
-type User = { id: number; name: string };
-
-const users: User[] = [
-  { id: 1, name: 'Alice' },
-  { id: 2, name: 'Bob' },
-  { id: 1, name: 'Alice' },
-];
-
-const uniqueUsers = Array.from(
-  new Map(users.map(u => [u.id, u])).values()
-); // [{ id: 1, name: 'Alice' }, { id: 2, name: 'Bob' }]
+// May error sa API
