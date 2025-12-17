671bc50b0b00c8b215999e7513c8a6933eebabc6
RyannKim327 <rksesgundo123@gmail.com>
2025-12-17 03:46:26 +0000

    12-17-2025 3:46 [Pinasaasa nya lang ako.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,9 @@
-// May error sa API
+const nums: number[] = [3, 5, 3, 7, 5, 3, 9];
+
+const unique = [...new Set(nums)];   // [3, 5, 7, 9]
+function unique<T>(arr: T[]): T[] {
+  return [...new Set(arr)];
+}
+
+const strings = ["a", "b", "a", "c"];
+console.log(unique(strings)); // ["a", "b", "c"]
