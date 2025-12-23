d95ee4591d729bb5a94d1716b275b47d6e260ba1
RyannKim327 <rksesgundo123@gmail.com>
2025-12-23 10:01:12 +0000

    12-23-2025 10:1 [Hindi naman daw ako pangit, pero ayaw nya kaya wag na]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,9 +1,1 @@
-const numbers: number[] = [5, 2, 100, 3, 8];
-
-// 1. Ascending
-numbers.sort((a, b) => a - b);
-console.log(numbers); // [2, 3, 5, 8, 100]
-
-// 2. Descending
-numbers.sort((a, b) => b - a);
-console.log(numbers); // [100, 8, 5, 3, 2]
+// May error sa API
