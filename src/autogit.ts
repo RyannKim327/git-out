7cfed3ca5ecd9c87e4389e6bc9221e89bbf6e567
RyannKim327 <rksesgundo123@gmail.com>
2025-12-24 15:00:55 +0000

    12-24-2025 15:0 [May pinagsamahan man tayo, pero tingin ko wala na un]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,8 +1,1 @@
-const first = "Hello";
-const last  = "World";
-
-// 1. Plus operator
-const combined1 = first + " " + last;   // "Hello World"
-
-// 2. Template literal (preferred for readability)
-const combined2 = `${first} ${last}`;     // "Hello World"
+// May error sa API
