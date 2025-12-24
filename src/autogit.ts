8891ab955741a6e591c4319a06f521926d24bf0c
RyannKim327 <rksesgundo123@gmail.com>
2025-12-24 05:14:09 +0000

    12-24-2025 5:14 [Wag nang umulit, at mukhang hindi na ako uulit pa]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,14 +1,1 @@
-function randomInt(min: number, max: number): number {
-  // inclusive min, exclusive max
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function randomFloat(min: number, max: number): number {
-  // inclusive min, exclusive max
-  return Math.random() * (max - min) + min;
-}
-
-// examples
-console.log(randomInt(1, 7));     // 1..6
-console.log(randomFloat(0, 1)); // 0 ≤ n < 1
-const inclusiveMax = Math.floor(Math.random() * (max - min + 1)) + min;
+// May error sa API
