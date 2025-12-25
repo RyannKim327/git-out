731816f5d7737834ef9d99aaf93efad54d894630
RyannKim327 <rksesgundo123@gmail.com>
2025-12-25 05:13:49 +0000

    12-25-2025 5:13 [Pinasaasa nya lang ako.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,6 +1,1 @@
-function removeVowels(str: string): string {
-  return str.replace(/[aeiouAEIOU]/g, '');
-}
-
-// Example usage:
-console.log(removeVowels("Hello World")); // "Hll Wrld"
+// May error sa API
