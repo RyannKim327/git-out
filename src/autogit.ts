9390b14b311e48d6caac217456c0983bd1ca52d9
RyannKim327 <rksesgundo123@gmail.com>
2025-12-28 21:00:42 +0000

    12-28-2025 21:0 [Mukhang kailangan ko nang i-practice na si sya ma miss]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,9 @@
-// May error sa API
+const numbers: number[] = [5, 2, 9, 1, 5, 6];
+
+// 1. Ascending
+numbers.sort((a, b) => a - b);
+console.log(numbers); // [1, 2, 5, 5, 6, 9]
+
+// 2. Descending
+numbers.sort((a, b) => b - a);
+console.log(numbers); // [9, 6, 5, 5, 2, 1]
