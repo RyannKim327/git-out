87c574ea3dbce953d39b0f059fe9ec4e976ffc6b
RyannKim327 <rksesgundo123@gmail.com>
2025-12-29 18:02:39 +0000

    12-29-2025 18:2 [Wag na ipaglaban kung gusto, basta masaya sya doon, don na sya]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,9 +1,1 @@
-const str = "  hello \t world \n";
-const noWhitespace = str.replace(/\s+/g, '');
-console.log(noWhitespace); // "helloworld"
-const str = "   hello world   ";
-const trimmed = str.trim();
-console.log(trimmed); // "hello world"
-const str = "hello     world    again";
-const normalized = str.replace(/\s+/g, ' ').trim();
-console.log(normalized); // "hello world again"
+// May error sa API
