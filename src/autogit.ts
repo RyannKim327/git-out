94ed9b7e12e7415aec6380104a99465b00364db4
RyannKim327 <rksesgundo123@gmail.com>
2025-12-31 03:54:01 +0000

    12-31-2025 3:54 [Nagcommit na din naman ako sa kanya, pero ayaw lang talaga nya]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,5 @@
-// May error sa API
+const reversed = (str: string): string => str.split('').reverse().join('');
+console.log(reversed('hello')); // "olleh"
+const reversed = (str: string): string => [...str].reverse().join('');
+// or
+const reversed = (str: string): string => Array.from(str).reverse().join('');
