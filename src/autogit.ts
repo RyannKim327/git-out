e6a0721a9c56f95c6f78017917cb446a2e7ca219
RyannKim327 <rksesgundo123@gmail.com>
2026-01-02 00:44:16 +0000

    1-2-2026 0:44 [Wag nang umulit, at mukhang hindi na ako uulit pa]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,10 @@
-// May error sa API
+const numbers: number[] = [3, 9, 2, 15, 7];
+
+const max = Math.max(...numbers); // 15
+console.log(max);
+function arrayMax(arr: number[]): number {
+  if (arr.length === 0) throw new Error('Empty array');
+  let m = arr[0];
+  for (let i = 1; i < arr.length; ++i) if (arr[i] > m) m = arr[i];
+  return m;
+}
