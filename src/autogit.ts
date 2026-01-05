5eb91568063f8b333ac4131c89b238ccbb505e3a
RyannKim327 <rksesgundo123@gmail.com>
2026-01-05 17:12:35 +0000

    1-5-2026 17:12 [Buti na lang umiwas na, kasi meron na pala]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,18 +1,1 @@
-const n = 42;
-const binary = n.toString(2);   // "101010"
-console.log(binary);
-function toBinary(n: number): string {
-  if (n === 0) return "0";
-
-  let result = "";
-  while (n > 0) {
-    result = (n & 1) + result; // or: (n % 2) + result
-    n >>>= 1;                    // unsigned right-shift
-  }
-  return result;
-}
-
-console.log(toBinary(42)); // "101010"
-const big = 123456789012345678901234567890n;
-const binary = big.toString(2);
-console.log(binary);
+// May error sa API
