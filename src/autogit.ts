5c811b473a6c02ad03ce08e963619ccaa7f3c9a1
RyannKim327 <rksesgundo123@gmail.com>
2026-01-05 09:18:56 +0000

    1-5-2026 9:18 [Paano yan, lugi nanaman tayo]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,4 +1,1 @@
-const nums = [1, 2, 3, 4, 5];
-nums.reverse();          // nums is now [5, 4, 3, 2, 1]
-const nums = [1, 2, 3, 4, 5];
-const reversed = [...nums].reverse(); // reversed = [5,4,3,2,1], nums unchanged
+// May error sa API
