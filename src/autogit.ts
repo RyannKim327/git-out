4ac0fc27702287e005a0f3c208266632791094e8
RyannKim327 <rksesgundo123@gmail.com>
2026-01-19 13:36:17 +0000

    1-19-2026 13:36 [Bakit mo pa kailangang mag code kung pwede namang matulog]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,30 +1,9 @@
-const a = [1, 2, 3, 4];
-const b = [3, 4, 5, 6];
-
-const intersection = a.filter(x => b.includes(x));
-console.log(intersection); // [3, 4]
-const a = [1, 2, 3, 4];
-const b = [3, 4, 5, 6];
-
-const setB = new Set(b);
-const intersection = a.filter(x => setB.has(x));
-console.log(intersection); // [3, 4]
-const a = [1, 2, 3, 4];
-const b = [3, 4, 5, 6];
-
-const [small, large] = a.length < b.length ? [a, b] : [b, a];
-const setSmall = new Set(small);
-
-const intersection = large.filter(x => setSmall.has(x));
-console.log(intersection); // [3, 4]
-const a = [1, 2, 2, 3, 4];
-const b = [2, 3, 3, 5];
-
-const intersection = Array.from(
-  new Set(a.filter(x => new Set(b).has(x)))
-);
-console.log(intersection); // [2, 3]
-import _ from 'lodash';
-
-const intersection = _.intersection([1, 2, 3], [2, 3, 4]);
-console.log(intersection); // [2, 3]
+function factorial(n: number): number {
+  if (n <= 1) return 1;   // base case: 0! = 1 and 1! = 1
+  return n * factorial(n - 1);
+}
+console.log(factorial(5)); // 120
+function factorialBig(n: bigint): bigint {
+  if (n <= 1n) return 1n;
+  return n * factorialBig(n - 1n);
+}
