670ea3b45b4d7364d112064d68a8b8a90cc54328
RyannKim327 <rksesgundo123@gmail.com>
2026-01-20 00:43:35 +0000

    1-20-2026 0:43 [Sanaol santol]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,7 +1,1 @@
-const haystack = 'hello world';
-const needle   = 'world';
-
-const contains = haystack.includes(needle); // true
-const containsIgnoreCase =
-  haystack.toLowerCase().includes(needle.toLowerCase());
-const contains = haystack.indexOf(needle) !== -1;
+// May error sa API
