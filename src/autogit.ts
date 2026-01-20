cad340f28923f09174ca1d08f08b2fa868059123
RyannKim327 <rksesgundo123@gmail.com>
2026-01-20 07:17:38 +0000

    1-20-2026 7:17 [Kung ang isda ay fish, bakit di ka miss?]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,10 +1,1 @@
-function removeVowels(str: string): string {
-  // /[aeiou]/i finds a, e, i, o, u in either case
-  return str.replace(/[aeiou]/gi, '');
-}
-console.log(removeVowels('Hello, World!')); // "Hll, Wrld!"
-function removeVowels(str: string): string {
-  const vowels = new Set('aeiouAEIOU');
-  return str.split('').filter(ch => !vowels.has(ch)).join('');
-}
-return str.replace(/[aeiouáéíóúAEIOUÁÉÍÓÚ]/gi, '');
+// May error sa API
