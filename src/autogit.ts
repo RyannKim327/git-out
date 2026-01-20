3ec073fc03c406112da14f99d0814bef6406713b
RyannKim327 <rksesgundo123@gmail.com>
2026-01-20 07:17:38 +0000

    1-20-2026 7:17 [Na hack ata github ko]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,10 @@
-// May error sa API
+function removeVowels(str: string): string {
+  // /[aeiou]/i finds a, e, i, o, u in either case
+  return str.replace(/[aeiou]/gi, '');
+}
+console.log(removeVowels('Hello, World!')); // "Hll, Wrld!"
+function removeVowels(str: string): string {
+  const vowels = new Set('aeiouAEIOU');
+  return str.split('').filter(ch => !vowels.has(ch)).join('');
+}
+return str.replace(/[aeiouáéíóúAEIOUÁÉÍÓÚ]/gi, '');
