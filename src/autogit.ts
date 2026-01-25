51a528ad32b9181ed7e2b4eb1dc3355253886ba6
RyannKim327 <rksesgundo123@gmail.com>
2026-01-25 15:00:59 +0000

    1-25-2026 15:0 [Na hack ata github ko]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,4 @@
-// May error sa API
+const original = "HeLLo WoRld!";
+const lower = original.toLowerCase();   // "hello world!"
+const turkish = "İSTANBUL";
+console.log(turkish.toLocaleLowerCase("tr"));  // "istanbul"
