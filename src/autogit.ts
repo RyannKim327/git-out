d8d3979b9a4c363ff197024c5a587cb8249e8446
RyannKim327 <rksesgundo123@gmail.com>
2026-01-28 02:54:51 +0000

    1-28-2026 2:54 [Friends with benefits daw muna kami sabi.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,9 +1,1 @@
-function factorial(n: number): number {
-  if (n <= 1) return 1;   // base case: 0! = 1 and 1! = 1
-  return n * factorial(n - 1);
-}
-console.log(factorial(5)); // 120
-function factorialBig(n: bigint): bigint {
-  if (n <= 1n) return 1n;
-  return n * factorialBig(n - 1n);
-}
+// May error sa API
