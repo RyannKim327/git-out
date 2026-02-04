c77ee42b2d8ae1b0176db5ab81ada7dd121254c1
RyannKim327 <rksesgundo123@gmail.com>
2026-02-04 18:30:14 +0000

    2-4-2026 18:30 [Na hack ata github ko]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,7 @@
-// May error sa API
+function safeParseInt(s: string, radix = 10): number | undefined {
+  const n = parseInt(s, radix);
+  return isNaN(n) ? undefined : n;
+}
+const intVal = parseInt(myStr, 10); // for ordinary integers
+const floatVal = Number(myStr);     // for decimals, natural format
+const alt = +myStr;                 // the one‑liner version of Number
