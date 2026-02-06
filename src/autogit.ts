221aa3340768cafdebae63d713267067720ba648
RyannKim327 <rksesgundo123@gmail.com>
2026-02-06 23:13:00 +0000

    2-6-2026 23:12 [Na hack ata github ko]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,5 +1,20 @@
-const arr = [1, 2, 3, 2, 4];
-const withoutTwos = arr.filter(x => x !== 2); // [1, 3, 4]
-const arr = [0, 1, 2, 3];
-const idx = 1;
-const sliced = [...arr.slice(0, idx), ...arr.slice(idx + 1)]; // [0, 2, 3]
+function reverseString(str: string): string {
+  return str.split('').reverse().join('');
+}
+function reverseStringLoop(str: string): string {
+  let out = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    out += str[i];
+  }
+  return out;
+}
+function reverseStringSpread(str: string): string {
+  return [...str].reverse().join('');
+}
+import GraphemeSplitter from 'grapheme-splitter';
+
+const splitter = new GraphemeSplitter();
+
+function reverseStringUnicode(str: string): string {
+  return splitter.splitGraphemes(str).reverse().join('');
+}
