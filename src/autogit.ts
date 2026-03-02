dc2228ea5c5ba062c4c45b7f12f0ef5f12df457b
RyannKim327 <rksesgundo123@gmail.com>
2026-03-02 22:15:46 +0000

    3-2-2026 22:15 [Mamaya na ko mag kocode ulit, tropa time muna.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,37 +1,7 @@
-// random-joke.ts
-import fetch from 'node-fetch';          // npm i node-fetch@2
-import { Console } from 'console';
-
-interface Joke {
-  id: number;
-  type: string;
-  setup: string;
-  punchline: string;
+const num = parseInt("123", 10);   // 123 as a number
+const num = Number("123");          // 123
+const num = +"123";                // 123
+const big = BigInt("123456789123456789123"); // 123456789123456789123n
+if (Number.isInteger(num)) {
+  // safe to use `num` as an int
 }
-
-async function fetchRandomJoke(): Promise<Joke> {
-  const res = await fetch('https://official-joke-api.appspot.com/random_joke');
-
-  if (!res.ok) {
-    throw new Error(`HTTP ${res.status} – failed to fetch joke`);
-  }
-
-  const data: Joke = await res.json();
-
-  return data;
-}
-
-async function run() {
-  try {
-    const joke = await fetchRandomJoke();
-
-    console.log('😂 Here’s something to make you smile!');
-    console.log(`  ${joke.setup}`);
-    console.log(`   – ${joke.punchline}`);
-  } catch (err: any) {
-    console.error('Oops! Something went wrong:');
-    console.error(err.message ?? err);
-  }
-}
-
-run();
