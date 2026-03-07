9e4e3a7a6fe8b03b2d31a97c3f5b76afdddee70e
RyannKim327 <rksesgundo123@gmail.com>
2026-03-07 17:10:10 +0000

    3-7-2026 17:10 [Friends with benefits daw muna kami sabi.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,5 @@
-// May error sa API
+const original = "Hello  world \t from TypeScript!";
+
+const noSpaces = original.replace(/\s+/g, '');   // → "HelloworldfromTypeScript!"
+const noSpaces = original.replace(/ +/g, '');
+const noSpaces = original.split(' ').join('');
