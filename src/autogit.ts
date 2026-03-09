f3c4ad945f432ed5bb1ea0498f3bfa877b7a9989
RyannKim327 <rksesgundo123@gmail.com>
2026-03-09 20:18:04 +0000

    3-9-2026 20:18 [Bakit may chatgpt dito?]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,7 +1,1 @@
-const num = parseInt("123", 10);   // 123 as a number
-const num = Number("123");          // 123
-const num = +"123";                // 123
-const big = BigInt("123456789123456789123"); // 123456789123456789123n
-if (Number.isInteger(num)) {
-  // safe to use `num` as an int
-}
+// May error sa API
