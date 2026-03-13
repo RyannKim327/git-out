2fd2ab93ee11fd7aa34bbe1e8fa93b458b9c7107
RyannKim327 <rksesgundo123@gmail.com>
2026-03-13 20:18:23 +0000

    3-13-2026 20:18 [Miss mo, kaso miss ka ba?]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,17 +1,1 @@
-const numbers = [1, 2, 3, 4];
-numbers.reverse();          // → [4, 3, 2, 1]
-console.log(numbers);       // the same array has changed
-const numbers = [1, 2, 3, 4];
-const reversed = numbers.slice().reverse(); // or [...numbers].reverse()
-
-console.log(numbers);   // → [1, 2, 3, 4]
-console.log(reversed);  // → [4, 3, 2, 1]
-function reverseArray<T>(arr: T[]): T[] {
-  const out = new Array<T>(arr.length);
-  for (let i = 0, j = arr.length - 1; i <= j; ++i, --j) {
-    out[i] = arr[j];
-    out[j] = arr[i];
-  }
-  return out;
-}
-const rev = reverseArray([10, 20, 30]); // → [30, 20, 10]
+// May error sa API
