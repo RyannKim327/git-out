7d870635f9837bb2861d4d56a28977484e91152e
RyannKim327 <rksesgundo123@gmail.com>
2026-03-14 11:13:06 +0000

    3-14-2026 11:13 [Friends with benefits daw muna kami sabi.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,12 +1,7 @@
-const numbers = [3, 5, 7, 9];
-const mean = numbers.reduce((sum, n) => sum + n, 0) / numbers.length;
-console.log(mean); // 6
-function mean(arr: number[]): number {
-  if (arr.length === 0) throw new Error('Cannot compute mean of empty array');
-
-  const total = arr.reduce((sum, val) => sum + val, 0);
-  return total / arr.length;
+const num = parseInt("123", 10);   // 123 as a number
+const num = Number("123");          // 123
+const num = +"123";                // 123
+const big = BigInt("123456789123456789123"); // 123456789123456789123n
+if (Number.isInteger(num)) {
+  // safe to use `num` as an int
 }
-
-// Usage
-const avg = mean([10, 20, 30]); // 20
