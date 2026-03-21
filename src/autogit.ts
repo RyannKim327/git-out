3b09a776fb2a9c0dac5506da8fb1e32d67607691
RyannKim327 <rksesgundo123@gmail.com>
2026-03-21 06:58:56 +0000

    3-21-2026 6:58 [Friends with benefits daw muna kami sabi.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,13 +1,1 @@
-const first = "Hello, ";
-const second = "world!";
-
-// Using + operator
-const greeting1 = first + second; // "Hello, world!"
-
-// Using a template literal
-const greeting2 = `${first}${second}`; // "Hello, world!"
-
-// Using concat()
-const greeting3 = first.concat(second); // "Hello, world!"
-
-console.log(greeting1, greeting2, greeting3);
+// May error sa API
