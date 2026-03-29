5dcf6729ed299f0d2700f0d9dfe76f02d43cd5e7
RyannKim327 <rksesgundo123@gmail.com>
2026-03-29 13:14:44 +0000

    3-29-2026 13:14 [Ekis iney-ay ang code.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,8 +1,1 @@
-const text = 'The quick brown fox';
-
-const containsFox = text.includes('fox');     // true
-const containsDog = text.includes('dog');     // false
-
-console.log(`fox? ${containsFox}, dog? ${containsDog}`);
-const idx = text.indexOf('brown');   // 10
-const missing = text.indexOf('cat'); // -1
+// May error sa API
