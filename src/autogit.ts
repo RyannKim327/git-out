49fde050ffc3c5e2b436f5950ef6a58f252f33bb
RyannKim327 <rksesgundo123@gmail.com>
2026-03-30 19:12:31 +0000

    3-30-2026 19:12 [Na hack ata github ko]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,11 @@
-// May error sa API
+function reverseString(str: string): string {
+  return str.split('').reverse().join('');
+}
+
+// Example
+console.log(reverseString('hello')); // 'olleh'
+const reverse = (s: string): string => [...s].reverse().join('');
+
+console.log(reverse('typescript')); // 'tpircysrat'
+const chars = ['a', 'b', 'c', 'd'];
+chars.reverse(); // ['d', 'c', 'b', 'a']
