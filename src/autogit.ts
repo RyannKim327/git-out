4910056bf90ef172d7e4394fd459e866ac92757a
RyannKim327 <rksesgundo123@gmail.com>
2026-04-01 13:57:02 +0000

    4-1-2026 13:57 [Ngingiti, tapos iiyak, ang gulo mo din e noh?]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,18 @@
-// May error sa API
+// file: square.ts
+import * as readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Give me a number: ", (answer: string) => {
+  const num = Number(answer);
+
+  if (!isNaN(num)) {
+    console.log(`⚡ ${num} × ${num} = ${num * num}`);
+  } else {
+    console.log("😕 That wasn’t a valid number!");
+  }
+  rl.close();
+});
