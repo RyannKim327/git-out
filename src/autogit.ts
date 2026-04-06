08eb9900e8abcd1d55152bb7b92352bc0fd622bb
RyannKim327 <rksesgundo123@gmail.com>
2026-04-06 19:13:43 +0000

    4-6-2026 19:13 []

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,14 @@
-// May error sa API
+function customLength(s: string): number {
+  let count = 0;
+  while (s[count] !== undefined) {
+    count++;
+  }
+  return count;
+}
+function customLengthUsingForOf(s: string): number {
+  let count = 0;
+  for (const _ of s) {
+    count++;
+  }
+  return count;
+}
