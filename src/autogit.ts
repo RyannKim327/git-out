4a206cffb7889a3a5a4545476560451caeebb5f5
RyannKim327 <rksesgundo123@gmail.com>
2026-04-06 13:40:27 +0000

    4-6-2026 13:39 [Ekis iney-ay ang code.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,14 +1,1 @@
-function customLength(s: string): number {
-  let count = 0;
-  while (s[count] !== undefined) {
-    count++;
-  }
-  return count;
-}
-function customLengthUsingForOf(s: string): number {
-  let count = 0;
-  for (const _ of s) {
-    count++;
-  }
-  return count;
-}
+// May error sa API
