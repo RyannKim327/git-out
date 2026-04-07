7683aedf0f96c6a0cb881e64e325aab0b953eb36
RyannKim327 <rksesgundo123@gmail.com>
2026-04-07 07:46:03 +0000

    4-7-2026 7:46 [Sanaol santol]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,13 +1,1 @@
-const numbers: number[] = [12, 5, 78, 3, 42];
-
-const maxValue = numbers.length
-  ? Math.max(...numbers)
-  : NaN;   // or throw, or return a sentinel value
-
-console.log(maxValue);   // 78
-function maxInArray<T>(arr: T[]): T | undefined {
-  if (!arr.length) return undefined; // or throw if you prefer
-  return arr.reduce((a, b) => (a > b ? a : b));
-}
-
-const max = maxInArray([12, 5, 78, 3, 42]); // 78
+// May error sa API
