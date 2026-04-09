8b0dd674960241e9cecf40c7e40e9580ce34b5e3
RyannKim327 <rksesgundo123@gmail.com>
2026-04-09 19:21:10 +0000

    4-9-2026 19:21 [Friends with benefits daw muna kami sabi.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,17 +1,1 @@
-interface ListNode<T> {
-    value: T;
-    next: ListNode<T> | null;
-}
-
-function length<T>(head: ListNode<T> | null): number {
-    let count = 0;
-    for (let curr = head; curr !== null; curr = curr.next) {
-        count++;
-    }
-    return count;
-}
-const node3: ListNode<number> = { value: 3, next: null };
-const node2: ListNode<number> = { value: 2, next: node3 };
-const node1: ListNode<number> = { value: 1, next: node2 };
-
-console.log(length(node1)); // 3
+// May error sa API
