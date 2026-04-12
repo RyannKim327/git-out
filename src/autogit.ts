d76f4cd4a600bbce9d6c4197c247d347cad173f4
RyannKim327 <rksesgundo123@gmail.com>
2026-04-12 03:38:00 +0000

    4-12-2026 3:37 [Ekis iney-ay ang code.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,9 @@
-// May error sa API
+const original = "HeLLo WoRLd";
+const lower = original.toLowerCase();
+
+console.log(lower); // "hello world"
+function toLowerCaseSafe(value: string | undefined | null): string {
+  return value?.toLowerCase() ?? "";
+}
+const turkish = "İstanbul";
+console.log(turkish.toLocaleLowerCase('tr-TR')); // "istanbul"
