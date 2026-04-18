c520f8fb04730445ccd3b0bdee661d422effd5b1
RyannKim327 <rksesgundo123@gmail.com>
2026-04-18 19:03:40 +0000

    4-18-2026 19:3 [Friends with benefits daw muna kami sabi.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,9 +1,1 @@
-// 1. The classic `+` operator
-const a = "hello";
-const b = "world";
-const combined1 = a + " " + b; // "hello world"
-
-// 2. Template literals (ES6+)
-const combined2 = `${a} ${b}`; // "hello world"
-const parts = [a, "awesome", b];
-const combined3 = parts.join(" "); // "hello awesome world"
+// May error sa API
