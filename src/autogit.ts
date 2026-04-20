b65c87c41f568d3a70fa99c15203aea3ad149682
RyannKim327 <rksesgundo123@gmail.com>
2026-04-20 19:20:18 +0000

    4-20-2026 19:19 [Ekis iney-ay ang code.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,9 +1,1 @@
-const str = "  123   ";
-
-const num1 = Number(str);            // 123
-const num2 = +'123';                 // 123
-const num3 = parseInt(str, 10);      // 123
-const num4 = parseFloat('123.45');   // 123.45
-const num5 = str * 1;                // 123
-
-console.log([num1, num2, num3, num4, num5]); // [123, 123, 123, 123.45, 123]
+// May error sa API
