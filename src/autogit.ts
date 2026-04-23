d56fe78c712d3351b8c6a8e7399d668f80c81a7f
RyannKim327 <rksesgundo123@gmail.com>
2026-04-23 19:25:50 +0000

    4-23-2026 19:25 [Ngingiti, tapos iiyak, ang gulo mo din e noh?]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,38 +1,16 @@
-interface ListNode<T> {
-  value: T;
-  next?: ListNode<T>;   // undefined means end of list
+function reverseString(str: string): string {
+  return str.split('').reverse().join('');
 }
-function listLength<T>(head: ListNode<T> | undefined): number {
-  let count = 0;
-  let current = head;
 
-  while (current) {
-    count++;
-    current = current.next;
+console.log(reverseString('hello')); // "olleh"
+function reverseStringManual(str: string): string {
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
   }
-
-  return count;
+  return result;
 }
-function listLengthRec<T>(node: ListNode<T> | undefined): number {
-  return node ? 1 + listLengthRec(node.next) : 0;
-}
-class LinkedList<T> {
-  head?: ListNode<T>;
-  tail?: ListNode<T>;
 
-  // ... push, pop, etc. ...
-
-  get length(): number {
-    let count = 0;
-    let cur = this.head;
-    while (cur) {
-      count++;
-      cur = cur.next;
-    }
-    return count;
-  }
-}
-const a: ListNode<number> = { value: 1, next: { value: 2, next: { value: 3 } } };
-
-console.log(listLength(a));          // 3
-console.log(listLengthRec(a));       // 3
+console.log(reverseStringManual('world')); // "dlrow"
+console.assert(reverseString('abc') === 'cba');
+console.assert(reverseStringManual('abc') === 'cba');
