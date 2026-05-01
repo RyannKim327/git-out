71a0a6c57d823d72af74da1676b058731af01006
RyannKim327 <rksesgundo123@gmail.com>
2026-05-01 19:23:01 +0000

    5-1-2026 19:23 [Miss mo, kaso miss ka ba?]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,9 @@
-// May error sa API
+const a = "Hello, ";
+const b = "world!";
+
+// 1. Using the + operator
+const joined1 = a + b;                 // "Hello, world!"
+
+// 2. Using template literals
+const joined2 = `${a}${b}`;            // also "Hello, world!"
+const joined3 = a.concat(b);           // "Hello, world!"
