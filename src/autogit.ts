bda8b58490e2316c415b2a96096534a5eb18764a
RyannKim327 <rksesgundo123@gmail.com>
2026-05-02 03:39:38 +0000

    5-2-2026 3:39 []

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,5 +1,1 @@
-const original = "a b  c\t\n  d";
-
-const noSpaces = original.replaceAll(/\s+/g, ""); // → "abcd"
-const noSpaces = original.replace(/\s+/g, "");
-const noSpaces = original.split(/\s+/).join("");
+// May error sa API
