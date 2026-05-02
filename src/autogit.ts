8333be1faea04177f00a5a9ebe494bb93ed49051
RyannKim327 <rksesgundo123@gmail.com>
2026-05-02 19:13:15 +0000

    5-2-2026 19:13 [Kung ang isda ay fish, bakit di ka miss?]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,5 @@
-// May error sa API
+const original = "a b  c\t\n  d";
+
+const noSpaces = original.replaceAll(/\s+/g, ""); // → "abcd"
+const noSpaces = original.replace(/\s+/g, "");
+const noSpaces = original.split(/\s+/).join("");
