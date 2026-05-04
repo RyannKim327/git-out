0f3b198236075ae930e78a7d460669d8227a813f
RyannKim327 <rksesgundo123@gmail.com>
2026-05-04 08:40:32 +0000

    5-4-2026 8:40 [Sanaol santol]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,16 @@
-// May error sa API
+function reverseString(str: string): string {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverseString('hello')); // "olleh"
+function reverseStringManual(str: string): string {
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  return result;
+}
+
+console.log(reverseStringManual('world')); // "dlrow"
+console.assert(reverseString('abc') === 'cba');
+console.assert(reverseStringManual('abc') === 'cba');
