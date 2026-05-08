6e12ed1c1a5868bd7bbe38558d5ec2c0b607d9cd
RyannKim327 <rksesgundo123@gmail.com>
2026-05-08 03:45:09 +0000

    5-8-2026 3:45 [Ngingiti, tapos iiyak, ang gulo mo din e noh?]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,18 @@
-// May error sa API
+function reverseSimple(str: string): string {
+  return str.split('').reverse().join('');
+}
+console.log(reverseSimple('hello')); // "olleh"
+function reverseWithSpread(str: string): string {
+  return [...str].reverse().join('');
+}
+function reverseManual(str: string): string {
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  return result;
+}
+function reverseUnicode(str: string): string {
+  return Array.from(str).reverse().join('');
+}
+console.log(reverseUnicode('👩‍👧‍👦')); // 👦🏽‍👧‍👩
