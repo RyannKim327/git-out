d5abb14ddf0656ff12063f2e283f2f91fa8c5963
RyannKim327 <rksesgundo123@gmail.com>
2026-06-03 05:03:51 +0000

    6-3-2026 5:3 [Ekis iney-ay ang code.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,15 +1,1 @@
-function reverseString(s: string): string {
-  // Split into an array of characters, reverse it, then join it back
-  return s.split('').reverse().join('');
-}
-// Install first: npm install grapheme-splitter
-import GraphemeSplitter from 'grapheme-splitter';
-
-const splitter = new GraphemeSplitter();
-
-function reverseGraphemeString(s: string): string {
-  const graphemes = splitter.splitGraphemes(s);
-  return graphemes.reverse().join('');
-}
-const reverseStringAlt = (s: string) =>
-  [...s].reduce((acc, ch) => ch + acc, '');
+// May error sa API
