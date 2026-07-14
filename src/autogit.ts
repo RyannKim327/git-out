441048f88e7eb83145eeca0bbb13f851c3dc45db
RyannKim327 <rksesgundo123@gmail.com>
2026-07-14 14:04:11 +0000

    7-14-2026 14:4 [Mamaya na ko mag kocode ulit, tropa time muna.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,45 +1,7 @@
-/**
- * Returns an object with the maximum sum and the start/end indices
- * of the sub‑array that produces that sum.
- *
- * @param nums - array of numbers
- * @returns { maxSum, start, end }
- */
-export function maxSumSubarray(nums: number[]) {
-  // In case the input is empty we can return 0 / -1/-1
-  if (nums.length === 0) {
-    return { maxSum: 0, start: -1, end: -1 };
-  }
+const part1 = "Hello";
+const part2 = "World";
 
-  let bestSum = nums[0];
-  let currentSum = nums[0];
-
-  // These will record the best sub‑array boundaries
-  let bestStart = 0;
-  let bestEnd = 0;
-  // Temporary positions
-  let tempStart = 0;
-
-  for (let i = 1; i < nums.length; i++) {
-    // Either extend the previous sub‑array or start fresh at i
-    if (currentSum + nums[i] < nums[i]) {
-      currentSum = nums[i];
-      tempStart = i;
-    } else {
-      currentSum += nums[i];
-    }
-
-    // Update best if we have a better sum
-    if (currentSum > bestSum) {
-      bestSum = currentSum;
-      bestStart = tempStart;
-      bestEnd = i;
-    }
-  }
-
-  return { maxSum: bestSum, start: bestStart, end: bestEnd };
-}
-const arr = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12];
-const { maxSum, start, end } = maxSumSubarray(arr);
-console.log(maxSum); // 43
-console.log(start, end); // 7 10 (sub‑array: [18, 20, -7, 12])
+const plus = part1 + part2;          // "HelloWorld"
+const concat = part1.concat(part2);  // "HelloWorld"
+const template = `${part1}${part2}`; // "HelloWorld"
+const join = [part1, part2].join(''); // "HelloWorld"
