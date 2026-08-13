04f7943824ad8383f5c462487e7d9d61e2afb6ba
RyannKim327 <rksesgundo123@gmail.com>
2026-08-13 07:45:05 +0000

    8-13-2026 7:45 [Miss mo, kaso miss ka ba?]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,9 @@
-// May error sa API
+const sentence = "The quick brown fox jumps over the lazy dog";
+const needle = "brown";
+
+const hasBrown = sentence.includes(needle); // true
+const hasBrownCaseInsensitive = sentence
+  .toLowerCase()
+  .includes(needle.toLowerCase()); // true
+const hasBrownIdx = sentence.indexOf(needle) !== -1; // true
+const hasVowelPattern = /[aeiou]/.test(sentence); // true
