e6b226d720729541e027b0c72366582f5212d840
RyannKim327 <rksesgundo123@gmail.com>
2026-08-19 18:45:35 +0000

    8-19-2026 18:45 [Friends with benefits daw muna kami sabi.]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,37 +1,7 @@
-function secondLargestSort(arr: number[]): number | undefined {
-  if (arr.length < 2) return undefined;
+const part1 = "Hello";
+const part2 = "World";
 
-  const sorted = [...arr].sort((a, b) => b - a); // Descending
-  return sorted[1];
-}
-function secondLargestSinglePass(arr: number[]): number | undefined {
-  if (arr.length < 2) return undefined;
-
-  let max = -Infinity;
-  let second = -Infinity;
-
-  for (const num of arr) {
-    if (num > max) {
-      second = max;
-      max = num;
-    } else if (num > second && num !== max) {
-      second = num;
-    }
-  }
-
-  return second === -Infinity ? undefined : second;
-}
-function secondLargestSet(arr: number[]): number | undefined {
-  const unique = [...new Set(arr)];
-  if (unique.length < 2) return undefined;
-
-  const sorted = unique.sort((a, b) => b - a);
-  return sorted[1];
-}
-function secondLargestMathMax(arr: number[]): number | undefined {
-  if (arr.length < 2) return undefined;
-
-  const max = Math.max(...arr);
-  const maxFiltered = arr.filter(num => num !== max);
-  return Math.max(...maxFiltered);
-}
+const plus = part1 + part2;          // "HelloWorld"
+const concat = part1.concat(part2);  // "HelloWorld"
+const template = `${part1}${part2}`; // "HelloWorld"
+const join = [part1, part2].join(''); // "HelloWorld"
