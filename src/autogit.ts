4226feabf1f1afbaed6018d767edbf38d93098d3
RyannKim327 <rksesgundo123@gmail.com>
2026-09-04 04:17:41 +0000

    9-4-2026 4:17 [Ngingiti, tapos iiyak, ang gulo mo din e noh?]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,7 @@
-// May error sa API
+const part1 = "Hello";
+const part2 = "World";
+
+const plus = part1 + part2;          // "HelloWorld"
+const concat = part1.concat(part2);  // "HelloWorld"
+const template = `${part1}${part2}`; // "HelloWorld"
+const join = [part1, part2].join(''); // "HelloWorld"
