64c3ac447ffcf9590b5c8f0b8c75acaf89d5d270
RyannKim327 <rksesgundo123@gmail.com>
2026-09-15 11:35:06 +0000

    9-15-2026 11:35 [Kung ang isda ay fish, bakit di ka miss?]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,1 +1,7 @@
-// May error sa API
+const removeSpaces = (s: string) => s.replace(/\s+/g, '');
+
+const original  = 'Hello   world!  How are   you?';
+const cleaned    = removeSpaces(original);
+
+console.log(cleaned); // "Helloworld!Howareyou?"
+const onlySpace = (s: string) => s.replace(/ +/g, '');
